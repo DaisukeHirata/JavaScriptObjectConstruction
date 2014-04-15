df5aef7996985d8d25481e3de54991db184271d6
Daisuke Hirata <dh@dhmbp.local>
2014-04-15 23:23:14 +0900

    ConstructorPattern added

---
+++ ConstructorPattern.js
@@ -0,0 +1,27 @@
+function Fruit (theColor, theSweetness, theFruitName, theNativeToLand) {
+
+    this.color = theColor;
+    this.sweetness = theSweetness;
+    this.fruitName = theFruitName;
+    this.nativeToLand = theNativeToLand;
+
+    this.showName = function () {
+        console.log("This is a " + this.fruitName);
+    }
+
+    this.nativeTo = function () {
+    this.nativeToLand.forEach(function (eachCountry)  {
+       console.log("Grown in:" + eachCountry);
+        });
+    }
+}
+
+var mangoFruit = new Fruit ("Yellow", 8, "Mango", ["South America", "Central America", "West Africa"]);
+mangoFruit.showName(); // This is a Mango.
+mangoFruit.nativeTo();
+//Grown in:South America
+// Grown in:Central America
+// Grown in:West Africa
+
+var pineappleFruit = new Fruit ("Brown", 5, "Pineapple", ["United States"]);
+pineappleFruit.showName(); // This is a Pineapple.
